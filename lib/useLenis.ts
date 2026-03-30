e8a359cd219c1e494c8f1c7baf3f38d13717717e
Mazigaming <mazito@wp.pl>
2026-03-30 02:08:40 +0200

    Fix Lenis smooth scroll - prevent content flashing on navigation

--- lib/useLenis.ts
+++ lib/useLenis.ts
@@ -3,31 +3,34 @@ import Lenis from 'lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+let lenisInstance: Lenis | null = null;
+
 export function useLenis() {
   useEffect(() => {
-    const lenis = new Lenis({
+    if (lenisInstance) {
+      lenisInstance.destroy();
+    }
+
+    lenisInstance = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
     });
 
-    lenis.on('scroll', ScrollTrigger.update);
+    lenisInstance.on('scroll', ScrollTrigger.update);
 
     gsap.ticker.add((time) => {
-      lenis.raf(time * 1000);
+      lenisInstance?.raf(time * 1000);
     });
 
     gsap.ticker.lagSmoothing(0);
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
     return () => {
-      lenis.destroy();
-      gsap.ticker.remove(raf);
+      if (lenisInstance) {
+        lenisInstance.destroy();
+        lenisInstance = null;
+      }
+      gsap.ticker.remove(() => {});
     };
   }, []);
 }
